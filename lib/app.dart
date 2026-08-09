9392bd0a41aa2cfeefbf34e3b3fe6e85e2c0c353
Abdelrahman Reda Ebrahim <balh99story@gmail.com>
2026-08-09 12:53:37 +0300

    fix: restore ProfileCubit in app.dart and di.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:z_sports_booking/core/di.dart';
 import 'package:z_sports_booking/core/router/app_router.dart';
 import 'package:z_sports_booking/core/theme/app_theme.dart';
-
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:z_sports_booking/core/di.dart';
 import 'package:z_sports_booking/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:z_sports_booking/features/profile/presentation/cubit/profile_cubit.dart';
 
 class ZSportsApp extends StatelessWidget {
   const ZSportsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AuthCubit(DI.authRepository),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => AuthCubit(DI.authRepository)),
+        BlocProvider(create: (_) => ProfileCubit(DI.profileRepository)),
+      ],
       child: MaterialApp.router(
         title: 'Z Sports',
         debugShowCheckedModeBanner: false,
--- lib/core/di.dart
+++ lib/core/di.dart
@@ -1,7 +1,9 @@
 import 'package:z_sports_booking/core/network/api_client.dart';
 import 'package:z_sports_booking/features/auth/data/repositories/auth_repository.dart';
+import 'package:z_sports_booking/features/profile/data/repositories/profile_repository.dart';
 
 class DI {
   static final ApiClient apiClient = ApiClient();
   static final AuthRepository authRepository = AuthRepository(apiClient);
+  static final ProfileRepository profileRepository = ProfileRepository(apiClient);
 }
